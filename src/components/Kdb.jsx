60e4c11da8e8450b3bd1def543bedad153ac2f6a
henmark560 <142089604+henmark560@users.noreply.github.com>
2023-08-19 11:59:37 +0100

    Update Kdb.jsx

--- src/components/Kdb.jsx
+++ src/components/Kdb.jsx
@@ -83,8 +83,8 @@ const [password, setPassword] = useState('')
 <section className="bg-gray-50 dark:bg-gray-900" id="login">
     <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
         <div className="flex flex-col justify-center">
-            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">UPSdeliver, we invest in the world’s potential</h1>
-            <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at UPSdeliver we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Novastrans, we invest in the world’s potential</h1>
+            <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Novastrans we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
             <a href="#" className="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">Read more about our app 
                 <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
